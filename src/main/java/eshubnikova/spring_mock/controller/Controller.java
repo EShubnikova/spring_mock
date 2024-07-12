9a105eaba04373af589912fe5b7553f4583873c1
EShubnikova <eugene.93@yandex.ru>
2024-07-12 13:55:27 +0300

    code refactor

--- src/main/java/eshubnikova/spring_mock/controller/Controller.java
+++ src/main/java/eshubnikova/spring_mock/controller/Controller.java
@@ -1,5 +1,9 @@
-package eshubnikova.spring_mock;
+package eshubnikova.spring_mock.controller;
 
+import eshubnikova.spring_mock.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,25 +14,29 @@ import java.util.Random;
 @RestController
 public class Controller {
 
-    @GetMapping("/login")
-    public Login login()  {
+    @GetMapping("/user")
+    public ResponseEntity<?> handleGetRequest() {
         try {
             Random random = new Random();
             Thread.sleep((long) ((random.nextDouble() + 1) * 1000));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            return new ResponseEntity(new User("username", "password"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new Login("username","password");
     }
 
-    @PostMapping("/login-date")
-    public LoginDate loginDate(@RequestBody Login login) {
+    @PostMapping("/user-date")
+    public ResponseEntity<?> handlePostRequest(@RequestBody User user) {
+        if (!user.usernameExists() || !user.passwordExists() || user.dateExists()) {
+            throw new HttpMessageNotReadableException("wrong JSON format"); }
         try {
             Random random = new Random();
             Thread.sleep((long) ((random.nextDouble() + 1) * 1000));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            return ResponseEntity.ok(new User(user.getUsername(), user.getPassword(), LocalDate.now()));
+        } catch (HttpMessageNotReadableException hmnre) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new LoginDate(login.getUsername(), login.getPassword(), LocalDate.now());
     }
 }
---
+++ src/main/java/eshubnikova/spring_mock/model/User.java
@@ -0,0 +1,42 @@
+package eshubnikova.spring_mock.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.time.LocalDate;
+
+public class User {
+    private String username;
+    private String password;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private LocalDate date;
+
+    public User() { }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public User(String username, String password, LocalDate date) {
+        this(username, password);
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public boolean usernameExists() {
+        return username != null;
+    }
+    public boolean passwordExists() {
+        return password != null;
+    }
+    public boolean dateExists() {
+        return date != null;
+    }
+}
