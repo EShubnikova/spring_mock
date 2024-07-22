799708302e1c8c06490d03d6cbb22b33eb4b83ea
EShubnikova <eugene.93@yandex.ru>
2024-07-22 19:18:05 +0300

    mock removed valid annotations

--- src/main/java/eshubnikova/spring_mock/model/User.java
+++ src/main/java/eshubnikova/spring_mock/model/User.java
@@ -1,14 +1,12 @@
 package eshubnikova.spring_mock.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.validation.constraints.NotBlank;
+
 
 import java.time.LocalDate;
 
 public class User {
-    @NotBlank(message = "username must not be blank")
     private String username;
-    @NotBlank(message = "password must not be blank")
     private String password;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private LocalDate date;
